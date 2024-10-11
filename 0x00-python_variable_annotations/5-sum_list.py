43986d49ddf2465c6edcb02183730483978947c3
root <mohammed2004koki@gmail.com>
2024-10-11 04:06:20 +0300

    tst

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,13 +2,14 @@
 """
 to string function
 """
+from typing import List
 
 
-def sum_list(list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Calculate the sum of all elements in a list
     """
     total = 0
-    for num in list:
+    for num in input_list:
         total += num
     return total
