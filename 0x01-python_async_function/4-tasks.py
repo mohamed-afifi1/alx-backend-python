fb061351f0e3773c46424f4505cda090d823827a
root <mohammed2004koki@gmail.com>
2024-10-14 23:46:54 +0300

    tst

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     wait a random number of seconds
     """
