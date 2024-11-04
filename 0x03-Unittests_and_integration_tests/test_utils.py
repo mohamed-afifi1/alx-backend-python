7cf0c355562c0f52b6397fb89d47b9696eb5b669
root <mohammed2004koki@gmail.com>
2024-11-04 02:55:01 +0200

    exception tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,12 @@ class TestAccessNestedMap(unittest.TestCase):
                                path: Tuple, expected: Union[int, Dict]):
         """test access nested map"""
         self.assertEqual(utils.access_nested_map(map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, map: Dict, path: Tuple):
+        """test access nested map with exception"""
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(map, path)
