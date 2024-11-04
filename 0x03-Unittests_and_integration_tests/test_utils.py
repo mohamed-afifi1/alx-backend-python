e8935b1b4a914d90ea292dde545c343b2bee816d
root <mohammed2004koki@gmail.com>
2024-11-04 02:51:07 +0200

    first tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,12 +4,18 @@ unittest for utils
 """
 import unittest
 import utils
+from parameterized import parameterized
+from typing import Dict, Tuple, Union
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    def test_access_nested_map(self):
-        self.assertEqual(utils.access_nested_map({"a": 1}, ["a"]), 1)
-        self.assertEqual(utils.access_nested_map({"a": {"b": 2}},
-                                                 ["a"]),  {"b": 2})
-        self.assertEqual(utils.access_nested_map({"a": {"b": 2}},
-                                                 ["a", "b"]), 2)
+    """test access nested map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, map: Dict,
+                               path: Tuple, expected: Union[int, Dict]):
+        """test access nested map"""
+        self.assertEqual(utils.access_nested_map(map, path), expected)
